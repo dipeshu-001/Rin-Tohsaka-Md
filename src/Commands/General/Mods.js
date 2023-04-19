1f65b2e5986396888cb0d0d3e2d8b55e1cc69f58
Toshi_さん <91844428+Toshi-san001@users.noreply.github.com>
2023-04-19 20:47:23 +0530

    Update Mods.js

--- src/Commands/General/Mods.js
+++ src/Commands/General/Mods.js
@@ -1,7 +1,6 @@
 const Command = require('../../Structures/Command')
 
 module.exports = class command extends Command {
-
    constructor() {
    super('mods', {
    description: 'Shows bot owner',
@@ -14,42 +13,22 @@ module.exports = class command extends Command {
         })
    }
 
-
-    }
-
- 
-
-  /**
-
-     * @param {Message} m
-
-=======
    /**
      * @param {Message} M
->>>>>>> 61272023bedabe4de46211efae87ecbe8ae7f4a7
      * @returns {Promise<void>}
      */
 
      execute = async (M) => {
-
-
-    execute = async (m) => {
-
-        if (!this.helper.config.mods.length) return void m.reply('*[UNMODERATED]*')
-
-        let text ="*❱❱❱❱❱ _MODS_ ❰❰❰❰❰*\n\n"
-
+     if (!this.helper.config.mods.length) return void M.reply('*[UNMODERATED]*')
+     let text ="*❱❱❱❱❱ MODS ❰❰❰❰❰*\n\n"
 
 const mentions = []
-
 this.helper.config.mods.map((x)=>{
 text+= `*❯ @${this.helper.contact.getContact(x).jid.split('@')[0]}*\n`
 mentions.push(this.helper.contact.getContact(x).jid)
 
 })
 
-        return void this.client.sendMessage(m.from,{text:text, mentions:mentions})
-
+        return void this.client.sendMessage(M.from,{text:text, mentions:mentions})
     }
-
 }
