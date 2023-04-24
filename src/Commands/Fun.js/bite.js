f2225814455ba241123115dd1af1b6397d99b019
Examinati <Aliwadood561@gmail.com>
2023-04-24 22:33:38 +0500

    Try React

---
+++ src/Commands/Fun.js/bite.js
@@ -0,0 +1,90 @@
+const Command = require('../../Structures/Command')
+const Message = require('../../Structures/Message')
+const axios = require('axios')
+
+module.exports = class command extends Command {
+    constructor() {
+        super('bite', {
+            description: 'Says hi to the bot',
+            category: 'general',
+            usage: 'gpt',
+            exp: 15,
+            cooldown: 5
+        })
+    }
+    
+    /**
+     * @param {Message} m
+     * @returns {Promise<void>}
+     */
+
+    execute = async (m , args) => { 
+
+        var pat = await this.helper.utils.fetchJson(`https://api.waifu.pics/sfw/bite`)
+
+        try {
+
+            let usep = m.sender.jid
+
+            let mentioneduser=``
+            let users = ''
+            let ment = ''
+   
+            try {
+      
+                const mention= m.mentioned
+     
+                users += await mention[0]
+    
+                ment+=[usep,users]
+  
+            } 
+            
+            catch {
+      
+                users += "none"
+      
+                ment+=[usep,m.sender]
+   
+            }
+   
+            if(users == 'none'){
+    
+                mentioneduser =`@${usep.split("@")[0]} bitten themselves`
+    
+                console.log(mentioneduser)
+    
+  
+            }
+            
+            else {
+   
+                const mentioneduserr =`@${users.split("@"[0])}`
+ 
+                mentioneduser= `@${usep.split("@")[0]} bitten @${users.split("@")[0]} `
+    
+                console.log(mentioneduser)
+   
+            }
+       
+            const response = await axios.get(pat.url,  { responseType: 'arraybuffer' })
+       
+            const buffer = Buffer.from(response.data, "utf-8")
+       
+            var sgif = await this.helper.utils.buffergif(buffer)
+       
+            client.sendMessage(m.from,{video: sgif, gifPlayback:true,mentions:ment,caption:mentioneduser},{quoted:m.message})
+      
+        }
+         
+        catch (error) {
+         
+            console.log(error);
+       
+        }
+   
+    }
+    
+  
+}
+    
--- src/lib/Utils.js
+++ src/lib/Utils.js
@@ -79,6 +79,29 @@ getCodeImage = async (code) => {
         return result;
       }
 
+
+      /**
+       * @param {string} url 
+       * @param {object} [options]
+       * @returns {Promise}
+       */
+
+      fetchJson = async (url, options) => {
+        try {
+            options ? options : {}
+            const res = await axios({
+                method: 'GET',
+                url: url,
+                headers: {
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'
+                },
+                ...options
+            })
+            return res.data
+        } catch (err) {
+            return err
+        }
+    }
     /**
      * @param {string} url
      * @returns {Promise<Buffer>}
