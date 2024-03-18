fba9e60d991745b49e4387e4b3fe8b35a872d913
Toshi_さん <91844428+Toshi-san001@users.noreply.github.com>
2024-03-19 01:31:23 +0530

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -16,6 +16,7 @@ const Auth = require('./Structures/Auth')
 const helper = new Helper({
     prefix: process.env.PREFIX || '+',
     name: process.env.NAME || 'Rin',
+    mongo: process.env.MONGO || 'mongodb+srv://chey:K77iYsuTRyJ4TgXf@cluster0.wtlbba3.mongodb.net/?retryWrites=true&w=majority',
     mods: (process.env.MODS || '').split(', ').map((jid) => `${jid}@s.whatsapp.net`),
     session: process.env.SESSION || 'XRE',
     PORT: (process.env.PORT || 3000)
